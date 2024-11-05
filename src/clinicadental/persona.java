01a117b1c8f8b2bc758cf1dc06bde29901a0e1eb
EAstuardo <ea9493088@gmail.com>
2024-11-05 15:17:40 -0600

    java clas

---
+++ src/clinicadental/persona.java
@@ -0,0 +1,104 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clinicadental;
+
+/**
+ *
+ * @author Other
+ */
+public class persona {
+    
+    private String nombre;
+    private int telefono;
+    private int dpi;
+    private String correo;
+    private int fecha;
+    private int contraseña;
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the telefono
+     */
+    public int getTelefono() {
+        return telefono;
+    }
+
+    /**
+     * @param telefono the telefono to set
+     */
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    /**
+     * @return the dpi
+     */
+    public int getDpi() {
+        return dpi;
+    }
+
+    /**
+     * @param dpi the dpi to set
+     */
+    public void setDpi(int dpi) {
+        this.dpi = dpi;
+    }
+
+    /**
+     * @return the correo
+     */
+    public String getCorreo() {
+        return correo;
+    }
+
+    /**
+     * @param correo the correo to set
+     */
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    /**
+     * @return the fecha
+     */
+    public int getFecha() {
+        return fecha;
+    }
+
+    /**
+     * @param fecha the fecha to set
+     */
+    public void setFecha(int fecha) {
+        this.fecha = fecha;
+    }
+
+    /**
+     * @return the contraseña
+     */
+    public int getContraseña() {
+        return contraseña;
+    }
+
+    /**
+     * @param contraseña the contraseña to set
+     */
+    public void setContraseña(int contraseña) {
+        this.contraseña = contraseña;
+    }
+    
+}

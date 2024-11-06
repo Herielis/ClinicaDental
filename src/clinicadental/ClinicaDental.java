f5a4cfa32249a1c0d77d354997d5accbbde1e3a0
Herielis <tajiboyherielis@gmail.com>
2024-11-05 22:49:52 -0600

    Hola

--- src/clinicadental/ClinicaDental.java
+++ src/clinicadental/ClinicaDental.java
@@ -5,9 +5,9 @@ public class ClinicaDental {
 
    
     public static void main(String[] args) {
-         Crear_usuario Crear_usuario;
-        Crear_usuario = new Crear_usuario();
-        Crear_usuario.setVisible(true);
+         Login Login;
+        Login = new Login();
+        Login.setVisible(true);
     }
     
 }
